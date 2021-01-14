b8a93bf058c82458a580e1ae1cfdb0d6b9554e87
JessK9 <60695082+JessK9@users.noreply.github.com>
2021-01-14 20:48:34 +0000

    updated

---
+++ editor-action.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    var codeEditorElement = $(".textarea")[0];
+    var code = document.getElementById("code-editor").value;
+
+    $(document).on('click', '#run', function(e) {
+        e.preventDefault();
+        $("#error").html("").hide();
+        var editorCode = code.getValue();
+
+        if(editorCode != '') {
+            $.ajax({
+                url: 'file-write.php',
+                type: 'POST',
+                dataType: 'json',
+                data: {"input":editorCode},
+                success:function(response) {},
+                complete:function(){
+                    $.ajax({
+                        url: 'code-editable.php',
+                        type: 'GET',
+                        success:function(response){
+                            console.log("response:  "+response);
+                            $("#result").html(response)	;
+                        },
+                        error:function(){
+                            console.log("error: "+response);
+                            }
+                    });
+                }
+            });
+        } else {
+            $("#error").html("Code should not be empty").show();
+
+        }
+
+    });
+});
